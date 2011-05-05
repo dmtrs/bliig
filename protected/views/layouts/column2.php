c37eb7e2890044c079d0e33b2fc994de4f391935
server@dimeng.home.gr <server@dimeng.home.gr>
2011-05-05 18:04:59 +0300

    removed the fedora banner

--- protected/views/layouts/column2.php
+++ protected/views/layouts/column2.php
@@ -11,24 +11,15 @@
 			<?php $this->widget('TagCloud', array(
 				'maxTags'=>Yii::app()->params['tagCloudCount'],
 			)); ?>
-			<?php 
-                        $this->beginWidget('zii.widgets.CPortlet', array(
+			<?php $this->beginWidget('zii.widgets.CPortlet', array(
                                 'title'=>'Links',
                         ));
-			$this->widget('zii.widgets.CMenu', array(
-				'items'=>array(
-					array('label'=>'Github repos','url'=>'http://www.github.com/dmtrs'),
-					array('label'=>'jsfiddle', 'url'=>'http://jsfiddle.net/user/tydeas_dr/'),
-					array('label'=>'Yii profile','url'=>'http://www.yiiframework.com/user/4786/'),
-				),
-				'htmlOptions'=>array('class'=>'mylinks')
-			));
 			$this->endWidget();?>
 
 
 			<?php $this->widget('RecentComments', array(
 				'maxComments'=>Yii::app()->params['recentCommentCount'],
-			)); ?><script id="fedora-banner" type="text/javascript" src="http://fedoraproject.org/static/js/release-counter-ext.js?lang=en"></script>
+			)); ?>
 		</div><!-- sidebar -->
 	</div>
 </div>
