1dc4c8442cca812e8ccc97dde2e513335a3ff08d
server@dimeng.home.gr <server@dimeng.home.gr>
2011-05-05 18:17:05 +0300

    added space to the log

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -23,7 +23,7 @@
 <div class="container" id="page">
 
 	<div id="header">
-		<div id="logo" style='position: absolute;'><?php echo CHtml::encode(Yii::app()->name); ?></div>
+		<div id="logo" ><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
 
 	<div id="mainmenu">
