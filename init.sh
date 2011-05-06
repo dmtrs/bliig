714ede99d520e713373b4a1b2f3c13cb07c4b9a1
Root on 38 <root@evresis.com>
2011-05-06 14:49:58 +0300

    init script added

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -v ./protected/data/_blog.db ./protected/data/blog.db
+cp -v ./protected/config/_main.php ./protected/config/main.php
+cp -v ./_index.php ./index.php
+cp -v ./_index-test.php ./index-test.php
