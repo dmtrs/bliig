c00148b796ccc58288660bb5252cc5e4f457187f
Root on 38 <root@evresis.com>
2011-05-10 12:10:56 +0300

    updated to cp params.php

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp -v ./protected/data/_blog.db ./protected/data/blog.db
 cp -v ./protected/config/_main.php ./protected/config/main.php
+cp -v ./protexted/config/_params.php ./protected/config/params.php
 cp -v ./_index.php ./index.php
 cp -v ./_index-test.php ./index-test.php
